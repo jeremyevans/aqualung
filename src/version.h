ce6505f45d691bf7c4ee014da623c87d58ca27a1
Tom Szilagyi <tszilagyi@users.sourceforge.net>
2005-01-28 15:37:59 +0000

    * last round of updating docs for beta4.
    git-svn-id: https://aqualung.svn.sourceforge.net/svnroot/aqualung/trunk@44 3cd24cdc-1f22-0410-b8b1-dcf80e670293

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-static const char * aqualung_version  = "0.122.0";
+static const char * aqualung_version  = "0.122.0 (released as 0.9beta4)";
 
 #endif /* _VERSION_H */

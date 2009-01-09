fad7c826dcf2d23e5208a01638ded96258e43a76
Peter Szilagyi <peterszilagyi@users.sourceforge.net>
2009-01-09 18:22:59 +0000

    Removed Philipp Überbacher from German translator.
    git-svn-id: https://aqualung.svn.sourceforge.net/svnroot/aqualung/trunk@1054 3cd24cdc-1f22-0410-b8b1-dcf80e670293

--- src/about.c
+++ src/about.c
@@ -175,7 +175,6 @@ create_about_window() {
 	gtk_text_buffer_insert_with_tags(buffer, &iter, _("Translators:"), -1, tag, NULL);
 	gtk_text_buffer_insert_at_cursor(buffer, "\n\n", -1);
 	gtk_text_buffer_insert_at_cursor(buffer, _("German:\n"), -1);
-	gtk_text_buffer_insert_at_cursor(buffer, "\tPhilipp \303\234berbacher <hollunder@gmx.at>\n", -1);
 	gtk_text_buffer_insert_at_cursor(buffer, "\tWolfgang St\303\266ggl <c72578@yahoo.de>\n\n", -1);
 	gtk_text_buffer_insert_at_cursor(buffer, _("Hungarian:\n"), -1);
 	gtk_text_buffer_insert_at_cursor(buffer, "\tPeter Szilagyi <peterszilagyi@users.sourceforge.net>\n\n", -1);

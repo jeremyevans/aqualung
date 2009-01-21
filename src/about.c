87a72b233f23ffb1a3dd63ab811e03e20969973f
Peter Szilagyi <peterszilagyi@users.sourceforge.net>
2009-01-21 16:12:17 +0000

    New translation by Niklas Grahn.
    git-svn-id: https://aqualung.svn.sourceforge.net/svnroot/aqualung/trunk@1056 3cd24cdc-1f22-0410-b8b1-dcf80e670293

--- src/about.c
+++ src/about.c
@@ -182,6 +182,8 @@ create_about_window() {
 	gtk_text_buffer_insert_at_cursor(buffer, "\tMichele Petrecca <michelinux@alice.it>\n\n", -1);
 	gtk_text_buffer_insert_at_cursor(buffer, _("Russian:\n"), -1);
 	gtk_text_buffer_insert_at_cursor(buffer, "\tAlexander Ilyashov <alexander.ilyashov@gmail.com>\n\n", -1);
+	gtk_text_buffer_insert_at_cursor(buffer, _("Swedish:\n"), -1);
+	gtk_text_buffer_insert_at_cursor(buffer, "\tNiklas 'Nixon' Grahn <terra.unknown@yahoo.com>\n\n", -1);
 	gtk_text_buffer_insert_at_cursor(buffer, _("Ukrainian:\n"), -1);
 	gtk_text_buffer_insert_at_cursor(buffer, "\tSergiy Niskorodov <sgh_punk@users.sourceforge.net>\n\n\n", -1);
 

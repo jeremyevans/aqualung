e325d2aa5729afd9f179c4c6c2610b22fc406647
Adam Sampson <ats@offog.org>
2020-07-19 03:09:26 +0100

    Add bounded wrappers for string functions.
    CHAR_ARRAY_SIZE checks that its argument is actually an array (given a
    C11 compiler) to prevent these functions from being used with a pointer
    as its destination.
    
    The wrappers include the names of the underlying functions they use to
    indicate what semantics to expect (e.g. strlcpy always null-terminates
    its result, unlike strncpy).

--- src/common.h
+++ src/common.h
@@ -28,6 +28,25 @@
 #define MAX_COLORNAME_LEN   32
 
 
+/* When built with a C11 compiler, CHAR_ARRAY_SIZE will cause an error if the
+   argument is not actually a char[] array. */
+#if __STDC_VERSION__ >= 201112L
+#define CHAR_ARRAY_SIZE(array) _Generic(&(array), char (*)[]: sizeof(array))
+#else
+#define CHAR_ARRAY_SIZE(array) sizeof(array)
+#endif
+
+/* Since Aqualung uses fixed-size C strings in many places, provide wrappers
+   for bounded string operations that use CHAR_ARRAY_SIZE to ensure it's an
+   array. */
+#define arr_strlcat(dest, src) \
+        g_strlcat((dest), (src), CHAR_ARRAY_SIZE(dest))
+#define arr_strlcpy(dest, src) \
+        g_strlcpy((dest), (src), CHAR_ARRAY_SIZE(dest))
+#define arr_snprintf(dest, ...) \
+        snprintf((dest), CHAR_ARRAY_SIZE(dest), __VA_ARGS__)
+
+
 #endif /* AQUALUNG_COMMON_H */
 
 // vim: shiftwidth=8:tabstop=8:softtabstop=8 :  
